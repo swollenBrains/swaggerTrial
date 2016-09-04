b707d516f970cc055b0fb409cf4701460f503bf7
unknown <swollenBrains@github.com>
2016-09-04 19:26:59 +0100

    tried springfox but didnt work

--- src/main/java/com/trials/service/MessageRestService.java
+++ src/main/java/com/trials/service/MessageRestService.java
@@ -5,9 +5,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value = "message")
 @Path("/message")
 public class MessageRestService {
 
+	 @ApiOperation(value = "Returns the message in url",
+			    notes = "Multiple status values can be provided with comma seperated strings")
 	@GET
 	@Path("/{param}")
 	public Response printMessage(@PathParam("param") String msg) {
